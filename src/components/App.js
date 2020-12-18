d7ee83bd6307663f86091a5bc170e65664ce1f07
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-18 15:51:22 +0000

    Fetch quotes on load and when the button is pressed

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import Button from "./Button";
 import Header from "./Header";
+import QuoteGrid from "./QuoteGrid";
+import fetchQuotes from "../fetchQuotes";
 import { CONTENT_WIDTH, SPACING } from "../constants";
 
 const StyledContentWrapper = styled.div`
@@ -12,12 +14,29 @@ const StyledContentWrapper = styled.div`
 `;
 
 const App = () => {
+  const [quotes, setQuotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const loadQuotes = async () => {
+    // TODO: Handle error state
+    setIsLoading(true);
+    const { data } = await fetchQuotes();
+    setQuotes(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadQuotes();
+  }, []);
+
   return (
     <>
       <Header />
 
       <StyledContentWrapper>
-        <Button>Load quotes</Button>
+        <Button onClick={() => loadQuotes()}>Load quotes</Button>
+
+        {isLoading ? <span>Loading...</span> : <QuoteGrid quotes={quotes} />}
       </StyledContentWrapper>
     </>
   );
