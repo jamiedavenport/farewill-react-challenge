bd073cb41c216a41dc16a556ca617a3c803ad169
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-18 13:18:45 +0000

    Setup Storybook for developing in isolation

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Button from '../components/Button';
+
+export default {
+  title: 'Farewill/Button',
+  component: Button,
+};
+
+export const Default = () => <Button>Test</Button>;
+
